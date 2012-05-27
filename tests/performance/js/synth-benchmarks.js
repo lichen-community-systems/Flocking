abb1f1d0b246edae0cf25c44c233bdfb1a834eb0
Colin Clark <colinbdclark@gmail.com>
2012-05-27 10:39:18 -0400

    Adds performance benchmarks for graphs of unit generators.

---
+++ tests/performance/js/synth-benchmarks.js
@@ -0,0 +1,79 @@
+/*
+* Flocking Synth Benchmark Tests
+* http://github.com/colinbdclark/flocking
+*
+* Copyright 2011, Colin Clark
+* Dual licensed under the MIT and GPL Version 2 licenses.
+*/
+
+/*jslint white: true, plusplus: true, undef: true, newcap: true, regexp: true, browser: true, 
+    forin: true, continue: true, nomen: true, bitwise: true, maxerr: 100, indent: 4 */
+
+var flock = flock || {};
+
+(function () {
+    "use strict";
+
+    flock.test = flock.test || {};
+
+    var makeSynthDefTestSpec = function (synthDef, rate, numSampleBlocks) {
+        return {
+            setup: function () {
+                var synth = flock.synth(synthDef),
+                    i,
+                    ugen;
+                
+                // Change the rate of each unit generator to the specified rate.
+                for (i = 0; i < synth.ugens.length; i++) {
+                    ugen = synth.ugens[i];
+                    if (ugen.id !== "audio-only") { // TODO: Craziness.
+                        ugen.rate = rate;
+                    }
+                }
+                
+                // And then whip through again and poke everyone to update their input assumptions.
+                for (i = 0; i < synth.ugens.length; i++) {
+                    ugen = synth.ugens[i];
+                    ugen.onInputChanged();
+                }
+                
+                return synth;
+            },
+        
+            test: function (synth) {
+                for (var i = 0; i < numSampleBlocks; i++) {
+                    synth.gen();    
+                }
+            }
+        };
+    };
+
+    flock.test.timeSynthDefs = function (name, synthDefs, rates, numSampleBlocks) {
+        var testSpecs = [],
+            testSpec,
+            i,
+            rate,
+            j;
+        
+        if (!flock.isArray(synthDefs)) {
+            synthDefs = [synthDefs];
+        }
+        
+        if (!flock.isArray(rates)) {
+            rates = [rates];
+        }
+    
+        for (i = 0; i < rates.length; i++) {
+            rate = rates[i];
+            
+            for (j = 0; j < synthDefs.length; j++) {
+                testSpec = makeSynthDefTestSpec(synthDefs[j], rate, numSampleBlocks);
+                testSpec.name = name + " " + rate + " rate.";
+                testSpecs.push(testSpec);
+            }
+        }
+    
+        sheep.tests(testSpecs, true);
+    };
+
+}());
