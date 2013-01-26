f06e7a4357a0e155b8219f7787cae5fb27a866a3
Colin Clark <colinbdclark@gmail.com>
2013-01-26 13:11:25 -0500

    Working non-Worker scheduler in Node.

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var fluid = require("infusion"),
     loader = fluid.getLoader(__dirname);
 
-fluid.setLogging(true);
-
 loader.require("./flocking/flocking/flocking-core.js");
 loader.require("./flocking-node.js");
 loader.require("./flocking/flocking/flocking-parser.js");
@@ -13,7 +11,3 @@ loader.require("./tests/test-synth.js")
 var flock = fluid.registerNamespace("flock");
 var synth = flock.demo.nodeTest();
 synth.play();
-
-setTimeout(function () {
-    // Wait 25 seconds before Node exits.
-}, 25000);
--- tests/test-synth.js
+++ tests/test-synth.js
@@ -6,30 +6,43 @@ fluid.registerNamespace("flock.demo");
 flock.demo.nodeTest = function () {
     flock.init({
         workerPath: "./flocking/flocking/flocking-worker.js",
-        bufferSize: 8192,
-        latency: 250
+        bufferSize: 64,
+        latency: 1
     });
+    
+    /*
+     * Multiple synths playing back simultaneously, playing shifting chords.
+     */
 
-    return flock.synth({
-        ugen: "flock.ugen.filter.biquad.hp",
-        freq: {
-            ugen: "flock.ugen.sin",
-            rate: "control",
-            freq: {
-                ugen: "flock.ugen.xLine",
-                rate: "control",
-                start: 0.7,
-                end: 10,
-                duration: 20
-            },
-            phase: 0,
-            mul: 3600,
-            add: 4000
-        },
-        source: {
-            ugen: "flock.ugen.lfSaw",
-            freq: 200,
-            mul: 0.1
-        }
+    // Creates an array of synths, each playing a degree of the chord specified in "intervals."
+    function makeIntervallicSynths (fundamental, intervals) {
+        var ampScale = 0.4 / intervals.length;
+        return fluid.transform(intervals, function (interval, idx) {
+            return flock.synth({
+                id: "carrier",
+                ugen: "flock.ugen.sin",
+                freq: fundamental * interval,
+                mul: ampScale
+            });
+        });
+    }
+
+    var fundamental = 440,
+        baseIntervals = [1/1, 5/4, 3/2],
+        weightedIntervals = baseIntervals.concat([4/3, 6/5, 7/6, 2/1]).concat(baseIntervals),
+        synths = makeIntervallicSynths(fundamental, baseIntervals),
+        synth = synths[0],
+        clock = flock.scheduler.async.beat(60);
+
+    // Every second, change one of the intervals by randomly choosing a synth
+    // and assigning it a new frequency from the list of intervals.
+    clock.repeat(1, function () {
+        var intervalSynth = flock.choose(synths),
+            newInterval = flock.choose(weightedIntervals)
+            newFreq = fundamental * newInterval;
+        fluid.log(newFreq);
+        intervalSynth.input("carrier.freq", newFreq);
     });
+    
+    return synth;
 };
