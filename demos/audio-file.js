e89afa526e5b1acddf68a9b29bc031c24515f3d1
Colin Clark <colinbdclark@gmail.com>
2013-08-29 14:41:26 -0400

    Adds full support for audio file decoding in Node.js, with demo.

--- demos/audio-file.js
+++ demos/audio-file.js
@@ -1,6 +1,5 @@
-var fs = require("fs"),
+var fluid = require("infusion"),
     //DSP = require("dsp"),
-    fluid = require("infusion"),
     loader = fluid.getLoader(__dirname);
 
 loader.require("../flocking/third-party/polydataview/js/polydataview.js");
@@ -14,56 +13,34 @@ loader.require("../flocking/flocking/flocking-parser.js");
 loader.require("../flocking/flocking/flocking-ugens.js");
 
 flock = fluid.registerNamespace("flock");
-fluid.registerNamespace("flock.file");
 
-// TODO: Refactor Flocking's file and buffer loading API to accomodate this.
-flock.file.readPathAsync = function (path, success, error) {
-    fs.exists(path, function (exists) {
-        if (!exists) {
-            console.log(path, "doesn't exist.");
-            return;
-        }
-        
-        fs.stat(path, function (error, stats) {
-            fs.open(path, "r", function (error, fd) {
-                var buf = new Buffer(stats.size);
-                fs.read(fd, buf, 0, buf.length, null, function (error, bytesRead) {
-                    var type = flock.file.parseFileExtension(path);
-                    var arr = new Int8Array(buf);
-                    var decoded = flock.audio.decodeArrayBuffer(arr.buffer, type);
-                    
-                    fs.close(fd);
-                    success(decoded, type);
-                });
-            });
-        });
-    })
-};
-
-flock.file.readPathAsync("demos/audio/hillier-first-chord.wav", function (bufferDesc) {
-    var synth = flock.synth({
-        synthDef: {
-            ugen: "flock.ugen.playBuffer",
-            buffer: bufferDesc.data.channels[0],
-            loop: 1,
-            speed: {
-                ugen: "flock.ugen.lfNoise",
-                freq: 2.5,
-                mul: {
-                    ugen: "flock.ugen.math",
-                    source: 1,
-                    div: {
-                        ugen: "flock.ugen.bufferDuration",
-                        buffer: bufferDesc.data.channels[0] // TODO: Improve buffer registration so this can be shared.
+var synth = flock.synth({
+    synthDef: {
+        ugen: "flock.ugen.playBuffer",
+        buffer: {
+            id: "hillier-first-chord",
+            url: "demos/audio/hillier-first-chord.wav"
+        },
+        loop: 1,
+        speed: {
+            ugen: "flock.ugen.lfNoise",
+            freq: 2.5,
+            mul: {
+                ugen: "flock.ugen.math",
+                source: 1,
+                div: {
+                    ugen: "flock.ugen.bufferDuration",
+                    buffer: {
+                        id: "hillier-first-chord"
                     }
-                },
-                add: 1.0,
-                options: {
-                    interpolation: "linear"
                 }
+            },
+            add: 1.0,
+            options: {
+                interpolation: "linear"
             }
         }
-    });
-    
-    synth.play();
+    }
 });
+
+synth.play();
