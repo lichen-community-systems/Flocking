6a02bd5c4af130aaa4e5e73d94b7a3b8bdf705a5
Colin Clark <colinbdclark@gmail.com>
2014-05-15 17:45:37 -0400

    Improved error messages when interpreting ugenDefs.

--- flocking/flocking-parser.js
+++ flocking/flocking-parser.js
@@ -16,18 +16,18 @@
 
 var fluid = fluid || require("infusion"),
     flock = fluid.registerNamespace("flock");
-    
+
 (function () {
     "use strict";
-    
+
     var $ = fluid.registerNamespace("jQuery");
     fluid.registerNamespace("flock.parse");
-    
+
     flock.parse.synthDef = function (ugenDef, options) {
         if (!ugenDef) {
             ugenDef = [];
         }
-        
+
         if (!flock.parse.synthDef.hasOutUGen(ugenDef)) {
             // We didn't get an out ugen specified, so we need to make one.
             ugenDef = {
@@ -44,28 +44,28 @@ var fluid = fluid || require("infusion"),
                 ugenDef.inputs.expand = options.audioSettings.chans;
             }
         }
-        
+
         return flock.parse.ugenForDef(ugenDef, options);
     };
-    
+
     flock.parse.synthDef.hasOutUGen = function (synthDef) {
         // TODO: This is hostile to third-party extension.
         return !flock.isIterable(synthDef) && (
-            synthDef.id === flock.OUT_UGEN_ID || 
-            synthDef.ugen === "flock.ugen.out" || 
+            synthDef.id === flock.OUT_UGEN_ID ||
+            synthDef.ugen === "flock.ugen.out" ||
             synthDef.ugen === "flock.ugen.valueOut"
         );
     };
-    
+
     flock.parse.makeUGen = function (ugenDef, parsedInputs, options) {
         var rates = options.audioSettings.rates,
             blockSize = options.audioSettings.blockSize;
-        
+
         // Assume audio rate if no rate was specified by the user.
         if (!ugenDef.rate) {
             ugenDef.rate = flock.rates.AUDIO;
         }
-    
+
         var sampleRate;
         // Set the ugen's sample rate value according to the rate the user specified.
         if (ugenDef.options && ugenDef.options.sampleRate !== undefined) {
@@ -73,7 +73,7 @@ var fluid = fluid || require("infusion"),
         } else {
             sampleRate = rates[ugenDef.rate];
         }
-        
+
         // TODO: Infusion options merging!
         ugenDef.options = $.extend(true, {}, ugenDef.options, {
             sampleRate: sampleRate,
@@ -86,23 +86,23 @@ var fluid = fluid || require("infusion"),
         // TODO: When we switch to Infusion options merging, these should have a mergePolicy of preserve.
         ugenDef.options.audioSettings.buffers = options.buffers;
         ugenDef.options.audioSettings.buses = options.buses;
-        
+
         var outputBufferSize = ugenDef.rate === flock.rates.AUDIO ? blockSize : 1,
             outputBuffers;
-        
+
         if (flock.hasTag(ugenDef.options, "flock.ugen.multiChannelOutput")) {
             var numOutputs = ugenDef.options.numOutputs || 1;
             outputBuffers = [];
-            
+
             for (var i = 0; i < numOutputs; i++) {
                 outputBuffers.push(new Float32Array(outputBufferSize));
             }
         } else {
             outputBuffers = new Float32Array(outputBufferSize);
         }
-        
+
         return flock.invoke(undefined, ugenDef.ugen, [
-            parsedInputs, 
+            parsedInputs,
             outputBuffers,
             ugenDef.options
         ]);
@@ -111,11 +111,11 @@ var fluid = fluid || require("infusion"),
 
     flock.parse.reservedWords = ["id", "ugen", "rate", "inputs", "options"];
     flock.parse.specialInputs = ["value", "buffer", "table"];
-    
+
     flock.parse.expandUGenDef = function (ugenDef) {
         var inputs = {},
             prop;
-       
+
         // Copy any non-reserved properties from the top-level ugenDef object into the inputs property.
         for (prop in ugenDef) {
             if (flock.parse.reservedWords.indexOf(prop) === -1) {
@@ -124,10 +124,10 @@ var fluid = fluid || require("infusion"),
             }
         }
         ugenDef.inputs = inputs;
-    
+
         return ugenDef;
     };
-    
+
     flock.parse.ugenDefForConstantValue = function (value) {
         return {
             ugen: "flock.ugen.value",
@@ -137,18 +137,19 @@ var fluid = fluid || require("infusion"),
             }
         };
     };
-    
+
     flock.parse.expandValueDef = function (ugenDef) {
         var type = typeof (ugenDef);
         if (type === "number") {
             return flock.parse.ugenDefForConstantValue(ugenDef);
         }
-        
+
         if (type === "object") {
             return ugenDef;
         }
-    
-        throw new Error("Invalid value type found in ugen definition.");
+
+        throw new Error("Invalid value type found in ugen definition. UGenDef was: " +
+            fluid.prettyPrintJSON(ugenDef));
     };
 
     flock.parse.rateMap = {
@@ -173,7 +174,7 @@ var fluid = fluid || require("infusion"),
         var parsed = parseFn(ugenDefs, options);
         return parsed;
     };
-    
+
     flock.parse.ugenDef.mergeOptions = function (ugenDef) {
         // TODO: Infusion options merging.
         var defaults = fluid.defaults(ugenDef.ugen) || {};
@@ -183,10 +184,10 @@ var fluid = fluid || require("infusion"),
         defaults.options = defaults.ugenOptions;
         delete defaults.ugenOptions;
         //
-        
+
         return $.extend(true, {}, defaults, ugenDef);
     };
-    
+
     flock.parse.ugensForDefs = function (ugenDefs, options) {
         var parsed = [],
             i;
@@ -206,7 +207,7 @@ var fluid = fluid || require("infusion"),
      *      - inputs: a JSON object containing named key/value pairs for inputs to the unit generator
      *           OR
      *      - inputs keyed by name at the top level of the ugenDef
-     * 
+     *
      * @param {UGenDef} ugenDef the unit generator definition to parse
      * @param {Object} options an options object containing:
      *           {Object} audioSettings the environment's audio settings
@@ -221,59 +222,60 @@ var fluid = fluid || require("infusion"),
             buses: flock.enviro.shared.buses,
             buffers: flock.enviro.shared.buffers
         }, options);
-        
+
         var o = options,
             visitors = o.visitors,
             rates = o.audioSettings.rates;
-         
+
         // If we receive a plain scalar value, expand it into a value ugenDef.
         ugenDef = flock.parse.expandValueDef(ugenDef);
-        
+
         // We received an array of ugen defs.
         if (flock.isIterable(ugenDef)) {
             return flock.parse.ugensForDefs(ugenDef, options);
         }
-    
+
         if (!ugenDef.inputs) {
             ugenDef = flock.parse.expandUGenDef(ugenDef);
         }
-        
+
         flock.parse.expandRate(ugenDef, options);
         ugenDef = flock.parse.ugenDef.mergeOptions(ugenDef, options);
-        
+
         var inputDefs = ugenDef.inputs,
             inputs = {},
             inputDef;
-        
+
         // TODO: This notion of "special inputs" should be refactored as a pluggable system of
         // "input expanders" that are responsible for processing input definitions of various sorts.
         // In particular, buffer management should be here so that we can initialize bufferDefs more
         // proactively and remove this behaviour from flock.ugen.buffer.
         for (inputDef in inputDefs) {
             var inputDefVal = inputDefs[inputDef];
-            
+
             if (inputDefVal === null) {
                 continue; // Skip null inputs.
             }
-            
+
             // Create ugens for all inputs except special inputs.
-            inputs[inputDef] = flock.input.shouldExpand(inputDef, ugenDef) ? 
+            inputs[inputDef] = flock.input.shouldExpand(inputDef, ugenDef) ?
                 flock.parse.ugenForDef(inputDefVal, options) : // Parse the ugendef and create a ugen instance.
                 inputDefVal; // Don't instantiate a ugen, just pass the def on as-is.
         }
-    
+
         if (!ugenDef.ugen) {
-            throw new Error("Unit generator definition lacks a 'ugen' property; can't initialize the synth graph.");
+            throw new Error("Unit generator definition lacks a 'ugen' property; " +
+                "can't initialize the synth graph. Value: " + fluid.prettyPrintJSON(ugenDef));
         }
-    
+
         var ugen = flock.parse.makeUGen(ugenDef, inputs, options);
         if (ugenDef.id) {
             ugen.id = ugenDef.id;
             ugen.nickName = ugenDef.id; // TODO: Normalize nicknames and ids.
         }
-        
+
         ugen.options.ugenDef = ugenDef;
-        
+
         if (visitors) {
             visitors = fluid.makeArray(visitors);
             fluid.each(visitors, function (visitor) {
@@ -283,7 +285,7 @@ var fluid = fluid || require("infusion"),
 
         return ugen;
     };
-    
+
     flock.parse.expandBufferDef = function (bufDef) {
         if (flock.isIterable(bufDef)) {
             // If we get a direct array reference, wrap it up in a buffer description.
@@ -293,16 +295,16 @@ var fluid = fluid || require("infusion"),
                 }
             });
         }
-        
+
         // If we get a bare string, interpret it as an id reference.
         return typeof (bufDef) !== "string" ? bufDef : {
             id: bufDef
         };
     };
-    
+
     flock.parse.bufferForDef = function (bufDef, ugen, enviro) {
         bufDef = flock.parse.expandBufferDef(bufDef);
-        
+
         if (bufDef.data && bufDef.data.channels) {
             flock.parse.bufferForDef.resolveBuffer(bufDef, ugen, enviro);
         } else {
@@ -312,7 +314,7 @@ var fluid = fluid || require("infusion"),
 
     flock.parse.bufferForDef.findSource = function (defOrDesc, enviro) {
         var source;
-        
+
         if (enviro && defOrDesc.id) {
             source = enviro.bufferSources[defOrDesc.id];
             if (!source) {
@@ -321,26 +323,26 @@ var fluid = fluid || require("infusion"),
         } else {
             source = flock.bufferSource();
         }
-        
+
         return source;
     };
-    
+
     flock.parse.bufferForDef.bindToPromise = function (p, source, ugen) {
         // TODO: refactor this.
         var success = function (bufDesc) {
-            source.events.onBufferUpdated.addListener(success);  
+            source.events.onBufferUpdated.addListener(success);
             if (ugen) {
                 ugen.setBuffer(bufDesc);
             }
         };
-        
+
         var error = function (msg) {
             throw new Error("Error while resolving buffer " + source.model.src + ": " + msg);
         };
-        
+
         p.then(success, error);
     };
-    
+
     flock.parse.bufferForDef.resolveDef = function (bufDef, ugen, enviro) {
         var source = flock.parse.bufferForDef.findSource(bufDef, enviro),
             p;
@@ -349,17 +351,17 @@ var fluid = fluid || require("infusion"),
         if (bufDef.selector && typeof(document) !== "undefined") {
             bufDef.src = document.querySelector(bufDef.selector).files[0];
         }
-        
+
         p = source.get(bufDef);
         flock.parse.bufferForDef.bindToPromise(p, source, ugen);
     };
-    
-    
+
+
     flock.parse.bufferForDef.resolveBuffer = function (bufDesc, ugen, enviro) {
         var source = flock.parse.bufferForDef.findSource(bufDesc, enviro),
             p = source.set(bufDesc);
-        
+
         flock.parse.bufferForDef.bindToPromise(p, source, ugen);
     };
-    
+
 }());
