03bec8138d1dc76c41dcf7a4651884e1829f34bd
Colin Clark <colinbdclark@gmail.com>
2014-08-09 13:01:14 -0400

    Upgrades to the latest node-speaker. Minor cleanup in use of Speaker.

--- dist/flocking-no-jquery.js
+++ dist/flocking-no-jquery.js
@@ -1,4 +1,4 @@
-/*! Flocking 0.1.0 (August 1, 2014), Copyright 2014 Colin Clark | flockingjs.org */
+/*! Flocking 0.1.0 (August 9, 2014), Copyright 2014 Colin Clark | flockingjs.org */
 
 (function (root, factory) {
     if (typeof exports === "object") {
