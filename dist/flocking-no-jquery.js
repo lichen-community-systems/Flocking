48b659a796efb193a6b15ddfa03319322098e1e9
Colin Clark <colinbdclark@gmail.com>
2014-11-26 20:54:53 -0500

    gh-44: Adds a sample-accurate input change scheduling unit generator.

--- dist/flocking-no-jquery.js
+++ dist/flocking-no-jquery.js
@@ -1,4 +1,4 @@
-/*! Flocking 0.1.0 (November 24, 2014), Copyright 2014 Colin Clark | flockingjs.org */
+/*! Flocking 0.1.0 (November 26, 2014), Copyright 2014 Colin Clark | flockingjs.org */
 
 (function (root, factory) {
     if (typeof exports === "object") {
