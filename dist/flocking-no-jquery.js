ef0997c30d7e4dc6a0f10d3493dc5c386d2ffe07
Colin Clark <colinbdclark@gmail.com>
2014-12-28 12:20:27 -0500

    gh-38: Uses the correct scheduler so that the demo isn't constantly running.

--- dist/flocking-no-jquery.js
+++ dist/flocking-no-jquery.js
@@ -1,4 +1,4 @@
-/*! Flocking 0.1.0 (December 21, 2014), Copyright 2014 Colin Clark | flockingjs.org */
+/*! Flocking 0.1.0 (December 28, 2014), Copyright 2014 Colin Clark | flockingjs.org */
 
 (function (root, factory) {
     if (typeof exports === "object") {
