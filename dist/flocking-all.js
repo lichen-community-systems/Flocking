623d8eec4dd9d2d2928a3ed4ec21003e76c32502
Colin Clark <colinbdclark@gmail.com>
2015-03-11 00:07:53 -0400

    Updates build files for 0.1.1.

--- dist/flocking-all.js
+++ dist/flocking-all.js
@@ -1,4 +1,4 @@
-/*! Flocking 0.1.0 (March 11, 2015), Copyright 2015 Colin Clark | flockingjs.org */
+/*! Flocking 0.1.1 (March 11, 2015), Copyright 2015 Colin Clark | flockingjs.org */
 
 /*!
  * jQuery JavaScript Library v2.1.3
