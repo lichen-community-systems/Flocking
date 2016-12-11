3bc6f7119c27cf188a9adcae0850dfb04a66d134
Colin Clark <colinbdclark@gmail.com>
2016-12-11 16:36:26 -0500

    gh-190: Updates built files.

--- dist/flocking-no-jquery.js
+++ dist/flocking-no-jquery.js
@@ -12593,7 +12593,7 @@ var fluid = fluid || require("infusion"),
     // TODO: Unit tests.
     fluid.defaults("flock.band", {
         gradeNames: ["fluid.component"],
-
+        synthGrade: "flock.noteTarget",
         invokers: {
             play: {
                 func: "{that}.events.onPlay.fire"
@@ -12606,10 +12606,9 @@ var fluid = fluid || require("infusion"),
             set: {
                 func: "{that}.events.onSet.fire"
             },
-
             getSynths: {
-                funcName: "flock.band.getSynths",
-                args: ["{that}"]
+                funcName: "fluid.queryIoCSelector",
+                args: ["{that}", "{that}.options.synthGrade"]
             }
         },
 
@@ -12653,17 +12652,6 @@ var fluid = fluid || require("infusion"),
             }
         }
     });
-
-    flock.band.getSynths = function (that) {
-        var synths = [];
-        fluid.each(that.options.components, function (componentDef, name) {
-            if (fluid.hasGrade(that[name].options, "flock.synth")) {
-                synths.push(that[name]);
-            }
-        });
-
-        return synths;
-    };
 }());
 ;/*
 * Flocking Audio Buffers
